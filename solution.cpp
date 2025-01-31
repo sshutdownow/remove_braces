7f875fd811f5538f3b05d4b3f0412fc4a9824296
Igor Popov <sshutdownow@users.noreply.github.com>
2025-01-31 12:54:29 +0200

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+long long
+burn_closed_braces(std::stack<char> &ops) {
+    long long ans = 0LL;
+
+    while (ops.size() >= 2) {
+        char op = ops.top();
+        ops.pop();
+      
+         // правильная скобочная последовательность
+        if (op == ')' && (op = ops.top()) == '(' ) {
+            ops.pop();
+            ans += 2LL;
+        } else {
+
+            long long n = burn_closed_braces(ops);
+            ops.push(op);
+            if (!n) {
+                break;
+            }
+            ans += n;
+        }
+    }
+
+    return ans;
+}
+
+int
+main(void) {
+    std::stack<char> ops;
+    int ch;
+
+    while ((ch = std::getchar()) != EOF ) {
+        switch (static_cast<char>(ch)) {
+           case '(':
+           case ')':
+              ops.push(ch);
+           default:
+               // ignore illegal chars
+              continue;
+       }
+    }
+    
+    size_t sz = ops.size();
+    std::cout <<  sz*1LL - burn_closed_braces(ops);
+    
+    return 0;
+
+}
